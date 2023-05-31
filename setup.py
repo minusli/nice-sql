28f0bace78d9369fe7ba74f91bd87b3b53330ac5
lichengming <lichengming@bytedance.com>
2023-05-31 15:33:34 +0800

    升级一个版本

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='nice-sql',
-    version='2.0.0',
+    version='2.0.1',
     author='minusli',
     author_email='minusli@foxmail.com',
     url='https://github.com/minusli/nicesql',
